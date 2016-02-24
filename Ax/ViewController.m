3617a1cb3dd1adf9d39b9faab956e7f1502c111e
cxy921126 <286193150@qq.com>
2016-02-24 23:25:07 +0800

    init

--- Ax/ViewController.m
+++ Ax/ViewController.m
@@ -9,6 +9,9 @@
 #import "ViewController.h"
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIButton *sighupBtn;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *frameMargin;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *loginMargin;
 
 @end
 
@@ -16,7 +19,27 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    self.sighupBtn.layer.cornerRadius = 5;
+//    self.sighupBtn.layer.masksToBounds = YES;
     // Do any additional setup after loading the view, typically from a nib.
+}
+- (IBAction)goSignIn:(id)sender {
+    self.loginMargin.constant = 8;
+    self.frameMargin.constant = -500;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+- (IBAction)backToOriginal:(id)sender {
+    self.loginMargin.constant = 1000;
+    self.frameMargin.constant = 8;
+    [UIView animateWithDuration:0.3 animations:^{
+        [self.view layoutIfNeeded];
+    }];
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
 }
 
 - (void)didReceiveMemoryWarning {
